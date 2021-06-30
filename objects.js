b7fcdc144b33be1d201ad975e172e1f45a672d10
JasperHX <jasper-hendrikx@hotmail.com>
2021-06-30 08:24:40 +0200

    homeworkklaar

--- objects.js
+++ objects.js
@@ -34,17 +34,25 @@ const product = {
 
 // 3a. Log de naam van de serie in de terminal.
 
+console.log(product.series)
+
 // 3b. Overschrijf de oude waarde van de property color ("creme") naar "roze". Log het object in de terminal om te checken of het gelukt is.
+
+product.color = 'roze';
+console.log(product.color);
 
 // 3c. Log de tweede materiaalsoort ("kunststof") in de terminal
 
+console.log(product.material[1]);
+
 // 1d. Log de afmetingen (b-h-d) in de terminal
 
+console.log(product['b-h-d']);
 
 // === Opdracht 4 ===
 // Declareer een variabele met daarin een string die het product beschrijft. Gebruik hiervoor de properties productName en material uit het object.
 // ---- Verwachte uitkomst:
 // "De Smeg Waterkoker is gemaakt van roestvrijstaal en kunststof"
 
-
-
+const nameDescription = ['De ' + product.productName + ' is gemaakt van ' + product.material[0] + product.material[1]]
+console.log(nameDescription);
